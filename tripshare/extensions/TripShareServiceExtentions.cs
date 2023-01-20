334287fec953730f2e94e6ce4c4150ac50489d73
williamchavula <rumbani@gmail.com>
2023-01-20 06:16:38 -0600

    add app services configuration extension method
    Signed-off-by: williamchavula <rumbani@gmail.com>

---
+++ tripshare/extensions/TripShareServiceExtentions.cs
@@ -0,0 +1,13 @@
+﻿namespace tripshare.extensions;
+
+public static class TripShareServiceExtentions
+{
+    public static MauiAppBuilder ConfigureTripShareServices(this MauiAppBuilder builder)
+    {
+        builder.Services.AddSingleton<IFetchDataClient<string>, FetchFromJsonFile>();
+        builder.Services.AddSingleton<IReadData, ReadDataFromLocalFileSource>();
+
+        return builder;
+    }
+}
+

bb44ba432696d969ee601e3ba4090852f7bd54f7
williamchavula <rumbani@gmail.com>
2023-01-20 05:27:43 -0600

    add configuration for fonts and font awesome
    Signed-off-by: williamchavula <rumbani@gmail.com>

--- tripshare/MauiProgram.cs
+++ tripshare/MauiProgram.cs
@@ -1,25 +1,33 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
 
 namespace tripshare;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+                //Custom Fonts
+                fonts.AddFont("Oxygen-Bold.ttf", "OxygenBold");
+                fonts.AddFont("Oxygen-Light.ttf", "OxygenLight");
+                fonts.AddFont("Oxygen-Regular.ttf", "OxygenRegular");
+                fonts.AddFont("fa-regular-400.ttf", "FaRegular");
+                fonts.AddFont("fa-solid-900.ttf", "FaSolid");
+            });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
 

0ab0e1bfa3613ffae2965d7c6a09bc838fb74fd2
williamchavula <rumbani@gmail.com>
2023-01-27 06:31:26 -0600

    Add unit test for loading accommodations behavior
    Signed-off-by: williamchavula <rumbani@gmail.com>

--- tripshare.tests/pagemodels/HomePageModelsTests.cs
+++ tripshare.tests/pagemodels/HomePageModelsTests.cs
@@ -2,18 +2,22 @@
 
 public class HomePageModelsTests : IClassFixture<TripsInitialStateFixture>
 {
-    readonly TripsInitialStateFixture _stateFixture;
+    private readonly TripsInitialStateFixture _stateFixture;
+    private readonly Mock<IReadData> mockIReadData;
+    private readonly Mock<IGetAccommodations> accommodations;
+
 
     public HomePageModelsTests(TripsInitialStateFixture stateFixture)
     {
         _stateFixture = stateFixture;
+        mockIReadData = new Mock<IReadData>();
+        accommodations = new Mock<IGetAccommodations>();
     }
 
     [Fact]
     public void ShouldInitializeTripsCollectionAtInstantiation()
     {
         // Arrange
-        var mockIReadData = new Mock<IReadData>();
         mockIReadData
             .Setup(
             instance => instance.LoadDataAsync(It.IsAny<string>()).Result
@@ -21,13 +25,51 @@ public class HomePageModelsTests : IClassFixture<TripsInitialStateFixture>
             .Returns(_stateFixture.Trips);
 
         // Act
-        var viewModel = new HomePageModel(mockIReadData.Object);
+        var viewModel = new HomePageModel(mockIReadData.Object, accommodations.Object);
 
         // Assert
         mockIReadData.Verify(
             instance => instance.LoadDataAsync("tripshare.assets.trips.json"),
             Times.Once());
         viewModel.Trips.Should().HaveCount(4);
+    }
+
+    [Fact]
+    public void ShouldInitializeAccommodationsCollectionAtInstantiation()
+    {
+        // Arrange
+        var accommodationsFixture = new ObservableCollection<Accommodation>
+        {
+            new Accommodation
+            {
+                AccommodationType = AccommodationType.Hotel,
+                Image = "test_hotel.png"
+            },
+            new Accommodation
+            {
+                AccommodationType = AccommodationType.Apartment,
+                Image = "test_apartment.png"
+            }
+        };
+        accommodations
+            .Setup(
+            instance => instance.LoadAccommodationsAsync().Result
+            )
+            .Returns(accommodationsFixture);
+
+        // Act
+        var viewModel = new HomePageModel(mockIReadData.Object, accommodations.Object);
+
+        // Assert
+        accommodations.Verify(
+            instance => instance.LoadAccommodationsAsync(),
+            Times.Once());
+
+        viewModel.Accommodations
+            .Should()
+            .NotBeEmpty()
+            .And.HaveCount(2)
+            .And.AllBeOfType<Accommodation>();
     }
 }
 

75edf7bdbfa9121709af0c2a1071ea04d61e1868
williamchavula <rumbani@gmail.com>
2023-01-27 06:27:39 -0600

    create a VacationControl custom control
    VacationControl is a reusable control to displace the accommodation type at a vacation destination, e.g. Hotel, Villa, etc.
    
    It displays the image and a caption label
    
    Signed-off-by: williamchavula <rumbani@gmail.com>

---
+++ tripshare/controls/VacationControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace tripshare.controls;
+
+public partial class VacationControl : StackLayout
+{
+
+
+    public string VacationImageAvatar
+    {
+        get => (string)GetValue(VacationImageAvatarProperty);
+        set => SetValue(VacationImageAvatarProperty, value);
+    }
+    public string VacationTypeName
+    {
+        get => (string)GetValue(VacationTypeNameProperty);
+        set => SetValue(VacationTypeNameProperty, value);
+    }
+
+    public VacationControl()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty VacationImageAvatarProperty = BindableProperty.Create(
+        propertyName: nameof(VacationImageAvatar),
+        returnType: typeof(string),
+        declaringType: typeof(VacationControl));
+
+
+
+    public static readonly BindableProperty VacationTypeNameProperty = BindableProperty.Create(
+        propertyName: nameof(VacationTypeName),
+        returnType: typeof(string),
+        declaringType: typeof(VacationControl));
+
+
+}

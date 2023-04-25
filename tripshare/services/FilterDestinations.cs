b729ffe459114202ff1ff79aae4d3ae9d64f1436
williamchavula <rumbani@gmail.com>
2023-04-24 20:02:03 -0500

    filter service implementation

---
+++ tripshare/services/FilterDestinations.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace tripshare.services;
+
+public class FilterDestinations : IFilterDestinations
+{
+    private readonly IDestinationService _destinationService;
+
+    public FilterDestinations(IDestinationService destinationService)
+    {
+        _destinationService = destinationService;
+    }
+
+    public async Task<IEnumerable<Destination>> ByAccommodationType(string accommodationType)
+    {
+        var allDestinations = await _destinationService.LoadDestinationsAsync();
+
+        var isFound = Enum.TryParse(accommodationType, out AccommodationType result);
+
+        if (!isFound)
+            return Array.Empty<Destination>();
+
+        return allDestinations.
+            Where(destination => destination.AccommodationTypes.
+            Contains(result));
+    }
+}
+

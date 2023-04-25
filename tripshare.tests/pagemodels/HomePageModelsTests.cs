a9dabaca2c364ad7f6a1f3dad8f33eea79f103df
williamchavula <rumbani@gmail.com>
2023-04-25 08:35:16 -0500

    add unit tests for LoadDestinations and LoadPromotions HomePageModel methods

--- tripshare.tests/pagemodels/HomePageModelsTests.cs
+++ tripshare.tests/pagemodels/HomePageModelsTests.cs
@@ -1,43 +1,32 @@
-﻿namespace tripshare.tests;
+﻿using tripshare.tests.fixtures;
 
-public class HomePageModelsTests : IClassFixture<TripsInitialStateFixture>
+namespace tripshare.tests;
+
+public class HomePageModelsTests : IClassFixture<TripsInitialStateFixture>, IClassFixture<DestinationsFixture>
 {
     private readonly TripsInitialStateFixture _stateFixture;
+    private readonly DestinationsFixture _destinationsFixture;
+
     private readonly Mock<IReadData> mockIReadData;
     private readonly Mock<IGetAccommodations> accommodations;
+    private readonly Mock<IDestinationService> destinations;
 
 
-    public HomePageModelsTests(TripsInitialStateFixture stateFixture)
+    public HomePageModelsTests(TripsInitialStateFixture stateFixture, DestinationsFixture destinationsFixture)
     {
         _stateFixture = stateFixture;
+        _destinationsFixture = destinationsFixture;
         mockIReadData = new Mock<IReadData>();
         accommodations = new Mock<IGetAccommodations>();
-    }
+        destinations = new Mock<IDestinationService>();
 
-    [Fact]
-    public void ShouldInitializeTripsCollectionAtInstantiation()
-    {
         // Arrange
         mockIReadData
             .Setup(
-            instance => instance.LoadDataAsync(It.IsAny<string>()).Result
+                instance => instance.LoadDataAsync(It.IsAny<string>()).Result
             )
             .Returns(_stateFixture.Trips);
 
-        // Act
-        var viewModel = new HomePageModel(mockIReadData.Object, accommodations.Object);
-
-        // Assert
-        mockIReadData.Verify(
-            instance => instance.LoadDataAsync("tripshare.assets.trips.json"),
-            Times.Once());
-        viewModel.Trips.Should().HaveCount(4);
-    }
-
-    [Fact]
-    public void ShouldInitializeAccommodationsCollectionAtInstantiation()
-    {
-        // Arrange
         var accommodationsFixture = new ObservableCollection<Accommodation>
         {
             new Accommodation
@@ -53,12 +42,37 @@ public class HomePageModelsTests : IClassFixture<TripsInitialStateFixture>
         };
         accommodations
             .Setup(
-            instance => instance.LoadAccommodationsAsync().Result
+                instance => instance.LoadAccommodationsAsync().Result
             )
             .Returns(accommodationsFixture);
 
+        destinations
+            .Setup(
+                instance => instance.LoadDestinationsAsync().Result
+            )
+            .Returns(_destinationsFixture.Destinations);
+    }
+
+    [Fact]
+    public void ShouldInitializeTripsCollectionAtInstantiation()
+    {
         // Act
-        var viewModel = new HomePageModel(mockIReadData.Object, accommodations.Object);
+        var viewModel = new HomePageModel(mockIReadData.Object, accommodations.Object, destinations.Object);
+        viewModel.Init(new object());
+
+        // Assert
+        mockIReadData.Verify(
+            instance => instance.LoadDataAsync("tripshare.assets.trips.json"),
+            Times.Once());
+        viewModel.Trips.Should().HaveCount(4);
+    }
+
+    [Fact]
+    public void ShouldInitializeAccommodationsCollectionAtInstantiation()
+    {
+        // Act
+        var viewModel = new HomePageModel(mockIReadData.Object, accommodations.Object, destinations.Object);
+        viewModel.Init(new object());
 
         // Assert
         accommodations.Verify(
@@ -70,6 +84,33 @@ public class HomePageModelsTests : IClassFixture<TripsInitialStateFixture>
             .NotBeEmpty()
             .And.HaveCount(2)
             .And.AllBeOfType<Accommodation>();
+    }
+
+    [Fact]
+    public void ShouldInitializeDestinationCollectionAtInstantiation()
+    {
+        // Act
+        var viewModel = new HomePageModel(mockIReadData.Object, accommodations.Object, destinations.Object);
+        viewModel.Init(new object());
+
+        // Assert
+        destinations.Verify(
+            instance => instance.LoadDestinationsAsync(),
+            Times.Once());
+        viewModel.Destinations.Should().HaveCount(10);
+    }
+
+    [Fact]
+    public void ShouldInitializePromotionsCollectionAtInstantiation()
+    {
+        // Act
+        var viewModel = new HomePageModel(mockIReadData.Object, accommodations.Object, destinations.Object);
+        viewModel.Init(new object());
+
+        // Assert
+        viewModel.Promotions.Should()
+            .NotBeNullOrEmpty()
+            .And.HaveCountGreaterThanOrEqualTo(1);
     }
 }
 

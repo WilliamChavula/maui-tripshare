79c8666b4024605d8607da6aa905526e53e121bd
williamchavula <rumbani@gmail.com>
2023-04-25 12:52:14 -0500

    add TripDetailPageModel tests

---
+++ tripshare.tests/pagemodels/TripDetailPageModelTests.cs
@@ -0,0 +1,31 @@
+﻿namespace tripshare.tests.pagemodels;
+
+[Collection("Destinations Collection")]
+public class TripDetailPageModelTests
+{
+    private readonly DestinationsFixture _destinationsFixture;
+
+    public TripDetailPageModelTests(DestinationsFixture destinationsFixture)
+    {
+        _destinationsFixture = destinationsFixture;
+    }
+
+    [Fact]
+    public void ShouldPopulatedDestinationPropertyOnInit()
+    {
+        // Arrange
+        var destination = _destinationsFixture.Destinations.FirstOrDefault();
+        var sut = new TripDetailPageModel();
+
+        // Act
+        sut.Init(destination);
+
+        // Assert
+        sut.Destination
+            .Should()
+            .NotBeNull()
+            .And.BeOfType<Destination>()
+            .And.BeEquivalentTo(destination);
+    }
+}
+

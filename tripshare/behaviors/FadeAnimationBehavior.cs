82c797ba51c0becee9789f9c3c5c06c3ab8b0caa
williamchavula <rumbani@gmail.com>
2023-04-28 17:55:19 -0500

    Create and Add Fade Animation Behavior to VisualElements

---
+++ tripshare/behaviors/FadeAnimationBehavior.cs
@@ -0,0 +1,51 @@
+﻿namespace tripshare.behaviors;
+
+public class FadeAnimationBehavior : Behavior<VisualElement>
+{
+    public static readonly BindableProperty PageProperty = BindableProperty.Create(
+        nameof(Page),
+        typeof(ContentPage),
+        typeof(FadeAnimationBehavior)
+        );
+
+    public FadeAnimationBehavior()
+    {
+    }
+
+    protected override void OnAttachedTo(VisualElement bindable)
+    {
+        if (Page is null)
+            throw new NullReferenceException($"Require property {nameof(Page)}. Please bind to enclosing ContentPage");
+
+        Page.Appearing += HandlePageAppearing(bindable);
+
+        base.OnAttachedTo(bindable);
+    }
+
+    public ContentPage Page
+    {
+        get => (ContentPage)GetValue(PageProperty);
+        set => SetValue(PageProperty, value);
+    }
+
+    protected override void OnDetachingFrom(VisualElement bindable)
+    {
+        base.OnDetachingFrom(bindable);
+    }
+
+    private EventHandler HandlePageAppearing(VisualElement bindable)
+    {
+        return delegate
+        {
+            Dispatcher.DispatchDelayed(TimeSpan.FromMilliseconds(200), () =>
+            {
+                bindable.Opacity = 0;
+                bindable.TranslationY = 10;
+
+                bindable.FadeTo(1, 2500, Easing.Linear);
+                bindable.TranslateTo(0, 0, 800, Easing.SinInOut);
+            });
+
+        };
+    }
+}

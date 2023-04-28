9d4e0f4af3599b1f7177612b3931919266eace33
williamchavula <rumbani@gmail.com>
2023-04-28 13:15:41 -0500

    Add custom animation fade and scale behavior on the Vacation Control

---
+++ tripshare/behaviors/FadeAndScaleAnimationBehavior.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Maui.Animations;
+
+namespace tripshare.behaviors;
+
+public class FadeAndScaleAnimationBehavior : BaseAnimation
+{
+    public double Scale { get; set; } = 0.6;
+    public double Opacity { get; set; } = 1;
+
+    public override async Task Animate(VisualElement view)
+    {
+        await Task.WhenAll(
+            view.ScaleTo(Scale, Length, Easing),
+            view.FadeTo(Opacity, Length, Easing)
+            );
+
+        await Task.WhenAll(
+            view.ScaleTo(1, Length, Easing),
+            view.FadeTo(1, Length, Easing)
+            );
+    }
+}

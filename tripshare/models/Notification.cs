5ab0d76ffa78b49a1c02231646a18e72be6d2d55
williamchavula <rumbani@gmail.com>
2023-05-16 18:27:44 -0500

    add IsRead property to Notification Model

--- tripshare/models/Notification.cs
+++ tripshare/models/Notification.cs
@@ -5,6 +5,7 @@ public class Notification
     public string Title { get; set; }
     public string Description { get; set; }
     public string ImageUrl { get; set; }
+    public bool IsRead { get; set; }
     public DateTime NotificationTime { get; set; }
 }
 

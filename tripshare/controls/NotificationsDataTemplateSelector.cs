046d79955696785fc5d163e6c94b14b10282b221
williamchavula <rumbani@gmail.com>
2023-05-16 20:46:47 -0500

    add template selector to switch between read/unread notifications

---
+++ tripshare/controls/NotificationsDataTemplateSelector.cs
@@ -0,0 +1,15 @@
+﻿namespace tripshare.controls;
+
+public class NotificationsDataTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate ReadNotification { get; set; }
+    public DataTemplate UnReadNotification { get; set; }
+
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        var notification = item as Notification;
+
+        return notification.IsRead ? ReadNotification : UnReadNotification;
+    }
+}
+

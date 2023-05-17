4a80ff69806add7d96276904d5925476d753e637
williamchavula <rumbani@gmail.com>
2023-05-16 22:41:26 -0500

    add Page and PageModel to display in-app notifications

--- tripshare/pagemodels/NotificationPageModel.cs
+++ tripshare/pagemodels/NotificationPageModel.cs
@@ -2,8 +2,45 @@
 
 public class NotificationPageModel : FreshBasePageModel
 {
-    public NotificationPageModel()
+    private readonly INotificationService service;
+
+
+    public NotificationPageModel(INotificationService notificationService)
     {
+        service = notificationService;
+    }
+
+    public ObservableCollection<Notification> Notifications { get; set; }
+
+    public Command<Notification> MarkAsReadCommand => new(notification =>
+    {
+        var oldNotifications = new ObservableCollection<Notification>(Notifications);
+
+        Notifications.Clear();
+
+        foreach (var item in oldNotifications)
+        {
+            if (item.NotificationId == notification.NotificationId)
+            {
+                item.IsRead = true;
+            }
+
+            Notifications.Add(item);
+        }
+    });
+
+    public Command CloseModalAsyncCommand => new(() =>
+    {
+        CoreMethods.PopPageModel(modal: true);
+    });
+
+    public override async void Init(object initData)
+    {
+        var notifications = await service.GenerateNotifications();
+
+        var ordered = notifications.OrderByDescending((arg) => arg.NotificationTime);
+
+        Notifications = new(ordered);
     }
 }
 

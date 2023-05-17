f0357065e78879361591f4cd16f28879348a17bb
williamchavula <rumbani@gmail.com>
2023-05-16 20:45:18 -0500

    Add converter to represent dates in human readable format

---
+++ tripshare/converter/DateToPrettyDateConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using tripshare.helpers;
+
+namespace tripshare.converter;
+
+public class DateToPrettyDateConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var notificationTime = ((DateTime)value).ToString();
+
+        return notificationTime.Prettify();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
